b0281438505b77fdf1a988d3b2a627a7fb79fe24
Nick <NRG626@yahoo.com>
2018-01-23 16:30:51 -0800

    For merge

---
+++ Windows/MusicPlayer/MusicPlayer/MusicPlayer/MainWindowSupportFunctions.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.Win32;
+using System.Windows.Media;
+
+namespace MusicPlayer
+{
+    public partial class MainWindow
+    {
+        public void PlayOrPauseMedia()
+        {
+            vlcPlayer.PauseOrResume();
+
+            if (IsPlaying)
+            {
+                icoPlayPause.Fill = new VisualBrush() { Visual = (Visual)Resources["appbar_control_play"] };
+                IsPlaying = false;
+            }
+            else
+            {
+                icoPlayPause.Fill = new VisualBrush() { Visual = (Visual)Resources["appbar_control_pause"] };
+                IsPlaying = true;
+            }
+        }
+
+        public void StopMedia()
+        {
+            vlcPlayer.Stop();
+        }
+
+        public void ChangeMediaVolume(int value)
+        {
+            if (vlcPlayer.Volume + value > 200)
+            {
+                vlcPlayer.Volume = 200;
+            }
+            else if (vlcPlayer.Volume + value < 0)
+            {
+                vlcPlayer.Volume = 0;
+            }
+            else
+            {
+                vlcPlayer.Volume += value;
+            }
+        }
+
+        //public void SkipNext()
+        //{
+        //    string file = playlistWindow.Next();
+
+        //    if (file != null)
+        //    {
+        //        LoadVideo(file);
+        //    }
+        //    else
+        //    {
+        //        return;
+        //    }
+        //}
+
+        //public void SkipPrevious()
+        //{
+        //    string file = playlistWindow.Previous();
+
+        //    if (file != null)
+        //    {
+        //        LoadVideo(file);
+        //    }
+        //    else
+        //    {
+        //        return;
+        //    }
+        //}
+
+        public void SelectNewSingleFile()
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Video Files|*.mp4;*.webm|Audio Files|*.mp3;*.wav|All Files|*.*";
+
+            if (ofd.ShowDialog() == true)
+            {
+                if (ofd.FileName != null)
+                {
+                    LoadVideo(ofd.FileName);
+                }
+            }
+        }
+
+        private void LoadVideo(string file)
+        {
+            vlcPlayer.LoadMedia(file);
+            vlcPlayer.Volume = (int)sldVolume.Value;
+            vlcPlayer.Play();
+            IsPlaying = true;
+            icoPlayPause.Fill = new VisualBrush() { Visual = (Visual)Resources["appbar_control_pause"] };
+        }
+    }
+}
---
+++ Windows/WindowsFileBrowser/WindowsFileBrowser/FileSystemListView.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace WindowsFileBrowser
+{
+    public class FileSystemListView: ListView
+    {
+        public FileSystemListView()
+        {
+            GridView gridView = new GridView();
+            gridView.AllowsColumnReorder = true;
+            gridView.ColumnHeaderToolTip = "Item Information";
+
+            GridViewColumn gvc1 = new GridViewColumn();
+            gvc1.DisplayMemberBinding = new Binding("Name");
+            gvc1.Header = "Name";
+            gvc1.Width = 300;
+            gridView.Columns.Add(gvc1);
+
+            GridViewColumn gvc2 = new GridViewColumn();
+            gvc2.DisplayMemberBinding = new Binding("Size");
+            gvc2.Header = "Size";
+            gvc2.Width = 100;
+            gridView.Columns.Add(gvc2);
+
+            GridViewColumn gvc3 = new GridViewColumn();
+            gvc3.DisplayMemberBinding = new Binding("FileType");
+            gvc3.Header = "Type";
+            gvc3.Width = 150;
+            gridView.Columns.Add(gvc3);
+
+            GridViewColumn gvc4 = new GridViewColumn();
+            gvc4.DisplayMemberBinding = new Binding("LastModified");
+            gvc4.Header = "Last Modified";
+            gvc4.Width = 150;
+            gridView.Columns.Add(gvc4);
+
+            this.View = gridView;
+
+        }
+    }
+}
